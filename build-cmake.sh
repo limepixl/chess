81f82221ae93a3a9118853317cdee814bd0b0eed
Stefan Ivanovski <stefs.ivanovski@tuta.io>
2021-05-06 16:26:29 +0200

    Changed num of threads

--- build-cmake.sh
+++ build-cmake.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cmake -S . -B build/ -DCMAKE_BUILD_TYPE=Release
-cmake --build build/thirdparty/tinyobjloader -j 4
-cmake --build build/thirdparty/SDL2 -j 4
-cmake --build build -j 4
+cmake --build build/thirdparty/tinyobjloader -j 2
+cmake --build build/thirdparty/SDL2 -j 2
+cmake --build build -j 2
